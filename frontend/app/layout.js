8f9aaf366b30375f33ab2e0938d8d6f617240d1b
= <=>
2026-04-20 14:59:23 +0500

    frontend updated

---
+++ frontend/app/layout.js
@@ -0,0 +1,28 @@
+import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Impact Tracker",
+  description: "Impact Tracker is a platform that helps you track your impact",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
