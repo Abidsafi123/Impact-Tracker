c6ce19e6c7e393b584ffae4f040aa650883d5f31
= <=>
2026-04-20 14:36:19 +0500

    project setup done

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+dotenv.config()
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
