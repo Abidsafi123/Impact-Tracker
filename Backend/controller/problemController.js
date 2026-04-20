d517734d18e1c07d9bb10e003a918a5ef0d08b00
= <=>
2026-04-20 15:44:21 +0500

    joining a problem and update problem status done

--- Backend/controller/problemController.js
+++ Backend/controller/problemController.js
@@ -84,4 +84,93 @@ export const createProblem = async(req,res)=>{
 }
 
 
-// join problems
+// join problem
+export const joinProlem = async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({
+        success: false,
+        message: "Name is required",
+      });
+    }
+
+    const { id } = req.params;
+
+    const problem = await problemModel.findById(id);
+
+    if (!problem) {
+      return res.status(404).json({
+        success: false,
+        message: "Problem not found",
+      });
+    }
+
+    // check if user already joined
+    if (!problem.members.includes(name)) {
+      problem.members.push(name);
+      problem.updates.push({text:`${name} joined the problem`})
+      await problem.save();
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Joined problem successfully",
+      problem,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error in joining problem",
+      error: error.message,
+    });
+  }
+};
+
+// update problem status
+
+export const updateProblemStatus = async (req, res) => {
+  try {
+    
+    const { id } = req.params;
+    const { status } = req.body;
+
+    if(!['pending','in-progress','completed'].includes(status)){
+      return res.status(400).json({
+        success: false,
+        message: "Invalid satatus value",
+      });
+
+      const problem = await problemModel.findById(id);
+      if (!problem) {
+        return res.status(404).json({
+          success: false,
+          message: "Problem not found",
+        });
+      }
+      problem.status = status;
+      problem.updates.push({ text: `Status changed to ${status}` });
+      await problem.save();
+      return res.status(200).json({
+        success: true,
+        message: "Status changed successfully",
+        problem,
+      });
+
+      
+    }
+
+    
+ 
+     
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error in joining problem",
+      error: error.message,
+    });
+  }
+};          
